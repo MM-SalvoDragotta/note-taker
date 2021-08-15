a521e22ac439cb47740aa5ba692b26030e6fbf76
Salvo Dragotta <salvo.dragotta@mottmac.com>
2021-08-15 11:11:22 +1000

    Add comments

--- routes/html.js
+++ routes/html.js
@@ -5,6 +5,7 @@ html.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
+// Wildcard route to direct users to index.html
 html.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
--- server.js
+++ server.js
@@ -1,14 +1,19 @@
+// Dependencies
 const express = require('express');
 const path = require('path');
 const api = require('./routes/index.js');
 
+// Point Server to the route files
 const PORT = process.env.port || 3001;
+// Create an express server
 const app = express();
 
+// Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/', api)
 
+app.use(express.static('public'));
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
