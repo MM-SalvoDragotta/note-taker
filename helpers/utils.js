9ee9a004f45c492753089b75aa9a2dcc98f31b18
Salvo Dragotta <salvo.dragotta@mottmac.com>
2021-08-15 16:50:36 +1000

    Implement getNotes() in utils

--- helpers/utils.js
+++ helpers/utils.js
@@ -1,22 +1,33 @@
-// const fs = require('fs');
-// const util = require('util');
-// const { v4: uuidv4 } = require('uuid');
-// const notesDb = require ('../db/db.json')
+const fs = require('fs');
+const util = require('util');
+const { v4: uuidv4 } = require('uuid');
+const notesDb = require ('../db/db.json')
 
-// // Promise version of fs.readFile
-// const readFromJson = util.promisify(fs.readFile);
-// const writeToJson = util.promisify(fs.writeFile);
+// Promise version of fs.readFile
+const readFromDb = util.promisify(fs.readFile);
+const writeToDb = util.promisify(fs.writeFile);
 
-// function getNotes () {
-// }
-
-// function newNote (body) {
-//     console.warn ('Implement new note')
-// }
-
-// function deleteNote(id) {
-//     console.warn ('Implement delete note') 
-// }
+function getNotes() {
+    return readFromDb('db/db.json', 'utf8')
+    .then(notes => { 
+        return JSON.parse(notes)   
+    });
+}
 
 
-// module.exports = { getNotes, newNote, deleteNote };
+
+
+
+
+function newNote (body) {
+    console.warn ('Implement new note')
+}
+
+function deleteNote(id) {
+    console.warn ('Implement delete note') 
+}
+
+
+// module.exports = new Utils()
+
+module.exports = {getNotes}
--- routes/notesRouter.js
+++ routes/notesRouter.js
@@ -2,23 +2,31 @@ const notes = require('express').Router();
 const fs = require('fs');
 const utils = require('../helpers/utils');
 
-// GET 
-notes.get('/notes',  (req, res) => {
-  console.info("GET")
+// GET This works. 
+// notes.get('/notes',  (req, res) => {
+//   console.info("GET")
 
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-      console.info(data)
-        if (err) {
-          console.warn(err);
-        } else {
-          // Convert string into JSON object
-          const parsedNotes = JSON.parse(data);
-          // parsed notes is the response
-          console.log(parsedNotes)
-          res.json(parsedNotes)
-        }
-      });
-    })
+//     fs.readFile('./db/db.json', 'utf8', (err, data) => {
+//       // console.info(data)
+//         if (err) {
+//           // console.warn(err);
+//         } else {
+//           // Convert string into JSON object
+//           const parsedNotes = JSON.parse(data);
+//           // parsed notes is the response
+//           // console.log(parsedNotes)
+//           res.json(parsedNotes)
+//         }
+//       });
+//     })
 
+
+notes.get('/notes', function (req, res) {
+  // console.info("GET")
+  // console.log(utils.getNotes())
+  utils.getNotes()
+    .then(notes => res.json(notes))
+    .catch(err => res.json(err));
+})   
 
 module.exports = notes;
